1ef2dcb1c1a9e82547c591adca948140102bd385
RumeshChathuranga <Rumarumesh1220@gmail.com>
2026-01-07 20:16:16 +0530

    Get user by id api

--- src/main/java/com/rumeshchathuranga/springapi/controllers/UserController.java
+++ src/main/java/com/rumeshchathuranga/springapi/controllers/UserController.java
@@ -4,17 +4,25 @@ import com.rumeshchathuranga.springapi.entities.User;
 import com.rumeshchathuranga.springapi.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/users")
 public class UserController {
 
     private final UserRepository userRepository;
 
-    @GetMapping("/users")
+    @GetMapping
     public Iterable<User> getUsers() {
         return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable Long id){
+        return  userRepository.findById(id).orElse(null);
     }
 }
