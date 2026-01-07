00a2eec0072757349c390cb2eb72315dccbfadc1
RumeshChathuranga <Rumarumesh1220@gmail.com>
2026-01-07 20:19:46 +0530

    Sending HTTP status codes

--- src/main/java/com/rumeshchathuranga/springapi/controllers/UserController.java
+++ src/main/java/com/rumeshchathuranga/springapi/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.rumeshchathuranga.springapi.controllers;
 import com.rumeshchathuranga.springapi.entities.User;
 import com.rumeshchathuranga.springapi.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +23,12 @@ public class UserController {
     }
 
     @GetMapping("/{id}")
-    public User getUser(@PathVariable Long id){
-        return  userRepository.findById(id).orElse(null);
+    public ResponseEntity<User> getUser(@PathVariable Long id){
+        var user = userRepository.findById(id).orElse(null);
+        if(user == null){
+//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(user);
     }
 }
