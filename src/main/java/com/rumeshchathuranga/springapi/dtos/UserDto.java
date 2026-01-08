5450833c9b64648788eade1ad4cf6cc22d645850
RumeshChathuranga <Rumarumesh1220@gmail.com>
2026-01-08 15:56:13 +0530

    Customizing responsedata

--- src/main/java/com/rumeshchathuranga/springapi/dtos/UserDto.java
+++ src/main/java/com/rumeshchathuranga/springapi/dtos/UserDto.java
@@ -1,12 +1,23 @@
 package com.rumeshchathuranga.springapi.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @Getter
 public class UserDto {
+    // @JsonIgnore -- for ignore Id
+    // @JsonProperty -- for rename id
     private Long id;
     private String name;
     private String email;
+//    @JsonInclude(JsonInclude.Include.NON_NULL)
+//    private String phoneNumber; -- response number if not null
+    @JsonFormat(pattern = "yyyy-mm-dd hh:mm:ss")
+    private LocalDateTime createdAt;
 }
--- src/main/java/com/rumeshchathuranga/springapi/mappers/UserMapper.java
+++ src/main/java/com/rumeshchathuranga/springapi/mappers/UserMapper.java
@@ -3,8 +3,10 @@ package com.rumeshchathuranga.springapi.mappers;
 import com.rumeshchathuranga.springapi.dtos.UserDto;
 import com.rumeshchathuranga.springapi.entities.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+    @Mapping(target = "createdAt", expression = "java(java.time.LocalDateTime.now())")
     UserDto toDto(User user);
 }
