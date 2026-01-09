6369552bfc92b647d311707b7ec93b7e7593bff9
RumeshChathuranga <Rumarumesh1220@gmail.com>
2026-01-09 15:52:19 +0530

    Handle action based updates

--- src/main/java/com/rumeshchathuranga/springapi/controllers/UserController.java
+++ src/main/java/com/rumeshchathuranga/springapi/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.rumeshchathuranga.springapi.controllers;
 
+import com.rumeshchathuranga.springapi.dtos.ChangePasswordRequest;
 import com.rumeshchathuranga.springapi.dtos.RegisterUserRequest;
 import com.rumeshchathuranga.springapi.dtos.UpdateUserRequest;
 import com.rumeshchathuranga.springapi.dtos.UserDto;
@@ -8,6 +9,7 @@ import com.rumeshchathuranga.springapi.mappers.UserMapper;
 import com.rumeshchathuranga.springapi.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -81,6 +83,24 @@ public class UserController {
         }
 
         userRepository.delete(user);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/{id}/change-password")
+    public ResponseEntity<Void> changePassword(
+            @PathVariable Long id,
+            @RequestBody ChangePasswordRequest request
+    ){
+        var user = userRepository.findById(id).orElse(null);
+        if(user == null){
+            return ResponseEntity.notFound().build();
+        }
+        if(!user.getPassword().equals(request.getOldPassword())){
+            return  new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        user.setPassword(request.getNewPassword());
+        userRepository.save(user);
+
         return ResponseEntity.noContent().build();
     }
 }
