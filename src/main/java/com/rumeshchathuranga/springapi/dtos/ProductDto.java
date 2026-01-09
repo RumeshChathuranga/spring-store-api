e890768262de64b850a413dab97b78c011a7f949
RumeshChathuranga <Rumarumesh1220@gmail.com>
2026-01-09 19:04:07 +0530

    handle product related requests

--- src/main/java/com/rumeshchathuranga/springapi/dtos/ProductDto.java
+++ src/main/java/com/rumeshchathuranga/springapi/dtos/ProductDto.java
@@ -2,11 +2,13 @@ package com.rumeshchathuranga.springapi.dtos;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 
 @AllArgsConstructor
 @Getter
+@Setter
 public class ProductDto {
     private Long id;
     private String name;
