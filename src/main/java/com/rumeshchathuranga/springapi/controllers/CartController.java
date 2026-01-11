a7303646dbd31a1cf3ccf62f0a263b52ef0269fd
RumeshChathuranga <Rumarumesh1220@gmail.com>
2026-01-11 16:32:57 +0530

    Clearing the cart

--- src/main/java/com/rumeshchathuranga/springapi/controllers/CartController.java
+++ src/main/java/com/rumeshchathuranga/springapi/controllers/CartController.java
@@ -107,4 +107,17 @@ public class CartController {
         cartRepository.save(cart);
         return ResponseEntity.noContent().build();
     }
+
+    @DeleteMapping("/{cartId}/items")
+    public ResponseEntity<Void> clearCart(@PathVariable UUID cartId){
+        var cart = cartRepository.getCartsWithItems(cartId).orElse(null);
+        if(cart == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        cart.clear();
+        cartRepository.save(cart);
+
+        return ResponseEntity.noContent().build();
+
+    }
 }
