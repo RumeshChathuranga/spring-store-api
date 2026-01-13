3a88fc2511d7aece72d830d51755dbc229bb019f
RumeshChathuranga <Rumarumesh1220@gmail.com>
2026-01-13 14:50:14 +0530

    Enhance JWT payload

--- src/main/java/com/rumeshchathuranga/springapi/controllers/AuthController.java
+++ src/main/java/com/rumeshchathuranga/springapi/controllers/AuthController.java
@@ -36,7 +36,8 @@ public class AuthController {
                 )
         );
 
-        var token = jwtService.generateToken(request.getEmail());
+        var user = userRepository.findByEmail(request.getEmail()).orElseThrow();
+        var token = jwtService.generateToken(user);
         return ResponseEntity.ok(new JwtResponse(token));
     }
     @PostMapping("/validate")
@@ -49,9 +50,9 @@ public class AuthController {
     @GetMapping("/me")
     public ResponseEntity<UserDto> me(){
         var authentication = SecurityContextHolder.getContext().getAuthentication();
-        var email = (String) Objects.requireNonNull(authentication).getPrincipal();
+        var userId = (Long) authentication.getPrincipal();
 
-        var user = userRepository.findByEmail(email).orElse(null);
+        var user = userRepository.findById(userId).orElse(null);
         if(user == null){
             return ResponseEntity.notFound().build();
         }
