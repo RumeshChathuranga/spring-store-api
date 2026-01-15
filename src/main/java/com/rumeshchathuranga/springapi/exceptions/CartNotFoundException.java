0889ba97a1eeb3d25c18055bd24b24a61650b56c
RumeshChathuranga <Rumarumesh1220@gmail.com>
2026-01-15 16:41:54 +0530

    Fix:CartNotFoundException.java

--- src/main/java/com/rumeshchathuranga/springapi/exceptions/CartNotFoundException.java
+++ src/main/java/com/rumeshchathuranga/springapi/exceptions/CartNotFoundException.java
@@ -1,7 +1,7 @@
 package com.rumeshchathuranga.springapi.exceptions;
 
-public class CartNotFoundException extends RuntimeException{
-    public CartNotFoundException(){
+public class CartNotFoundException extends RuntimeException {
+    public CartNotFoundException() {
         super("Cart not found");
     }
 }
