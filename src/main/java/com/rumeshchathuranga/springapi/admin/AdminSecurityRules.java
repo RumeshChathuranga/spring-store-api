90b61c3b1e5a37e9c06347e9974bf5ff5360f93c
RumeshChathuranga <Rumarumesh1220@gmail.com>
2026-01-16 15:24:11 +0530

    MOdularize security rules

---
+++ src/main/java/com/rumeshchathuranga/springapi/admin/AdminSecurityRules.java
@@ -0,0 +1,15 @@
+package com.rumeshchathuranga.springapi.admin;
+
+import com.rumeshchathuranga.springapi.common.SecurityRules;
+import com.rumeshchathuranga.springapi.users.Role;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AdminSecurityRules implements SecurityRules {
+    @Override
+    public void configure(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry registry) {
+        registry.requestMatchers("/admin/**").hasRole(Role.ADMIN.name());
+    }
+}
